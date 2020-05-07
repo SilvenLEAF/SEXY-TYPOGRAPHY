41f201de1021e18ed3acae07311f48930061a0a2
SilvenLEAF <silvenleaf@gmail.com>
2020-05-07 13:06:33 +0530

    inserted images through JS

---
+++ Carousel/javascript.js
@@ -0,0 +1,9 @@
+document.querySelector('.one').src = 'images/Glory-LOGO.jpg';
+document.querySelector('.two').src = 'images/Glory-Ye-Xiu.jpg';
+document.querySelector('.three').src = 'images/Glory-Heroine.jpg';
+document.querySelector('.four').src = 'images/Glory-Heroine-Friend.jpg';
+document.querySelector('.five').src = 'images/Glory-Friend.jpg';
+document.querySelector('.six').src = 'images/Glory-Math-Genius.jpg';
+document.querySelector('.seven').src = 'images/Glory-Muscular.jpg';
+document.querySelector('.eight').src = 'images/Glory-Villain.jpg';
+document.querySelector('.nine').src = 'images/Glory-Mage-Apprentice.jpg';
